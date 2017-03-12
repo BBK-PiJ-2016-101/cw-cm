0120cc7e629085f87c19769390af3f52e5c2741d
Jacob Williams <jwilli38@mail.bbk.ac.uk>
2017-03-12 20:48:41 +0000

    Check in ContactImpl.java

---
+++ src/main/java/impl/ContactImpl.java
@@ -0,0 +1,33 @@
+public class ContactImpl {
+    int id;
+    String name;
+    String notes;
+
+    public Contact(int id, String name) {
+	this.id = id;
+	this.name = name;
+	this.notes = "";
+    }
+    
+    public Contact(int id, String name, String notes) {
+	this.id = id;
+	this.name = name;
+	this.notes = notes;
+    }
+
+    public int getId() {
+	return this.id;
+    }
+
+    public String getName() {
+	return this.name;
+    }
+
+    public String getNotes() {
+	return this.notes;
+    }
+
+    public void addNotes(String note) {
+	this.notes = this.notes + "\n" + note;
+    }
+}
