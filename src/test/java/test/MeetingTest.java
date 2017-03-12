06d29a5bee4e39ee44a5b71d2df8a8584f9383ca
Jacob Williams <jwilli38@mail.bbk.ac.uk>
2017-03-12 22:43:35 +0000

    Flawed tests of an abstract class turning up issues with other abstract classes

--- src/test/java/test/MeetingTest.java
+++ src/test/java/test/MeetingTest.java
@@ -1,1 +1,19 @@
+import org.junit.*;
+import static org.junit.Assert.*;
 
+import java.util.Set;
+import java.util.Calendar;
+
+public class MeetingTest {
+    MeetingImpl testMeeting;
+
+    @Before
+    public void testSetup() {
+	Calendar testCalendar = new Calendar();
+	testCalendar.set(2017,03,19,23,59);
+	Set<spec.Contact> testContacts = new Set<spec.Contact>();
+	ContactImpl firstContact = new ContactImpl(1,"John Doe", "Unidentified male");
+	ContactImpl secondContact = new ContactImpl(2,"Jane Doe", "Unidentified female");
+	MeetingImpl testMeeting = new MeetingImpl(1,testCalendar,testContacts);
+    }
+}
