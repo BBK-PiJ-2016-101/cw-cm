e01340a7d1c24ce3af5cab58ef4da87b233585a5
Jacob Williams <jwilli38@mail.bbk.ac.uk>
2017-03-13 23:17:03 +0000

    Test a mock MeetingImpl object

--- src/test/java/test/MeetingTest.java
+++ src/test/java/test/MeetingTest.java
@@ -2,18 +2,45 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 import java.util.Set;
+import java.util.HashSet;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import spec.Meeting;
 
 public class MeetingTest {
-    MeetingImpl testMeeting;
+    Calendar testDate = new GregorianCalendar();
+    Set<spec.Contact> testContacts = new HashSet<spec.Contact>();
+    /*
+    ContactImpl firstContact = new ContactImpl(1,"John Doe", "Unidentified male");
+    ContactImpl secondContact = new ContactImpl(2,"Jane Doe", "Unidentified female");
+    */
+    MeetingMock MeetingMockTest = new MeetingMock(1,testDate,testContacts);
 
+    /*
     @Before
-    public void testSetup() {
-	Calendar testCalendar = new Calendar();
-	testCalendar.set(2017,03,19,23,59);
-	Set<spec.Contact> testContacts = new Set<spec.Contact>();
-	ContactImpl firstContact = new ContactImpl(1,"John Doe", "Unidentified male");
-	ContactImpl secondContact = new ContactImpl(2,"Jane Doe", "Unidentified female");
-	MeetingImpl testMeeting = new MeetingImpl(1,testCalendar,testContacts);
+    private void testSetup() {
+    testDate.set(2017,03,19,23,59);
+    testContacts.add(firstContact);
+    testContacts.add(secondContact);
+
     }
+    */
+
+    @Test
+    public void testMeetingId() {
+	assertEquals(1,MeetingMockTest.getId());
+    }
+
+    @Test
+    public void testMeetingDate() {
+	Calendar compareDate = new GregorianCalendar();
+	compareDate.set(2017,03,19,23,59);
+	assertEquals(compareDate,MeetingMockTest.getDate());
+    }
+
+    @Test
+    public void testMeetingContacts() {
+	assertEquals(2,MeetingMockTest.getContacts().size());
+    }
+
 }
