b76267ed3af983534283daf334dc7b1990d3ac5c
Jacob Williams <jwilli38@mail.bbk.ac.uk>
2017-03-13 23:23:22 +0000

    Implement FutureMeeting

---
+++ src/main/java/impl/FutureMeeting.java
@@ -0,0 +1,8 @@
+import java.util.Set;
+import java.util.Calendar;
+
+public class FutureMeetingImpl extends Meeting {
+    public FutureMeetingImpl(int id, Calendar date; Set<spec.Contact> contacts) {
+	super(id,date,contacts);
+    }
+}
